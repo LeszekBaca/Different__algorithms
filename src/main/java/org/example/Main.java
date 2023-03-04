a05ba7c5c39e2f1f2788b36874d0b1b6cb9f0eb0
unknown <leszek.baca@op.pl>
2023-03-04 22:27:29 +0100

    different algorithms

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.example.Functions.solution;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(32));
+        System.out.println(solution(15));
+        System.out.println(solution(1041));
+        System.out.println(solution(328));
+        System.out.println(solution(5));
+    }
+}
