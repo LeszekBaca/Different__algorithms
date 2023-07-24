4dd975001fac3b1436eb52550142f173342daa82
Leszek Baca <leszek.baca@op.pl>
2023-07-24 21:29:43 +0200

    Reformat code

--- src/main/java/org/example/Functions.java
+++ src/main/java/org/example/Functions.java
@@ -10,9 +10,7 @@ public class Functions {
 
     /**
      * Write a function that returns the sum of two numbers.
-     * <p>
      * Example:
-     * <p>
      * For param1 = 1 and param2 = 2, the output should be add(param1, param2) =
      * 3.
      */
